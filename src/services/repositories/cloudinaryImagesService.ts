9ed44c730bc7d8fbba54f4b05c5e282bd3be359e
leoFagundes <leofagundes2015@gmail.com>
2025-06-01 16:28:40 -0300

    fix: resolve image limit size error  with imageCompression

--- src/services/repositories/cloudinaryImagesService.ts
+++ src/services/repositories/cloudinaryImagesService.ts
@@ -1,26 +1,53 @@
+import imageCompression from "browser-image-compression";
+
 export const uploadImageToCloudinary = async (
   file: File,
   uploadPreset: string,
   cloudName: string
 ): Promise<string> => {
-  const formData = new FormData();
-  formData.append("file", file);
-  formData.append("upload_preset", uploadPreset);
+  const tamanhoMaximoMB = 10;
+  const tamanhoMaximoBytes = tamanhoMaximoMB * 1024 * 1024;
 
-  const response = await fetch(
-    `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-    {
-      method: "POST",
-      body: formData,
+  const options = {
+    maxSizeMB: 2,
+    maxWidthOrHeight: 1920,
+    useWebWorker: true,
+  };
+
+  try {
+    const compressedBlob = await imageCompression(file, options);
+
+    const compressedFile = new File([compressedBlob], file.name, {
+      type: compressedBlob.type,
+      lastModified: Date.now(),
+    });
+
+    if (compressedFile.size > tamanhoMaximoBytes) {
+      throw new Error("Imagem não pode ser maior do que 10MB.");
     }
-  );
 
-  if (!response.ok) {
-    throw new Error("Erro ao fazer upload da imagem");
+    const formData = new FormData();
+    formData.append("file", compressedFile);
+    formData.append("upload_preset", uploadPreset);
+
+    const response = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Erro ao fazer upload da imagem");
+    }
+
+    const data = await response.json();
+    return data.secure_url;
+  } catch (error) {
+    console.error("Erro ao comprimir ou enviar a imagem:", error);
+    throw error;
   }
-
-  const data = await response.json();
-  return data.secure_url; // URL final da imagem
 };
 
 export const deleteImageFromCloudinary = async (
@@ -47,9 +74,7 @@ export const deleteImageFromCloudinary = async (
     }
   );
 
-  console.log("PUBLIC ID PARA DELETAR:", publicId);
   const result = await response.json();
-  console.log("Cloudinary delete result:", result);
 
   if (result.result !== "ok") {
     // throw new Error(`Erro ao deletar imagem: ${result.result}`);
