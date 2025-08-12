9a156c25af80ac6ed131148bf26e1f955eb5d95c
leoFagundes <leofagundes2015@gmail.com>
2025-08-12 19:32:59 -0300

    feat: add resend feature

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -2,12 +2,14 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+resend.domains.verify("d91cd9bd-1176-453e-8fc1-35364d380206");
+
 export async function POST(req) {
   try {
     const { to, subject, message } = await req.json();
 
     const data = await resend.emails.send({
-      from: "Carcassonne <onboarding@resend.dev>",
+      from: "Carcassonne <carcassonnepub@gmail.com>",
       to,
       subject: subject,
       html: `<div>${message}</div>`,
