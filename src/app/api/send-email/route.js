bb69c597f044963d23892a585363b396448f5496
leoFagundes <leofagundes2015@gmail.com>
2025-08-12 21:03:21 -0300

    feat: add resend feature

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -1,24 +1,47 @@
 import {
   ClientReservationEmail,
   StaffReservationEmail,
-} from "@/emails/Templates";
+} from "@/components/react-email/clientResponseTemplate";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+resend.domains.verify("d91cd9bd-1176-453e-8fc1-35364d380206");
 
 export async function POST(req) {
-  const { to, subject, props, template } = await req.json();
+  try {
+    const { to, subject, props } = await req.json();
 
-  const component =
-    template === "staff" ? (
-      <StaffReservationEmail {...props} />
-    ) : (
-      <ClientReservationEmail {...props} />
+    const component =
+      template === "staff" ? (
+        <StaffReservationEmail {...props} />
+      ) : (
+        <ClientReservationEmail {...props} />
+      );
+
+    const data = await resend.emails.send({
+      from: "Carcassonne Pub <reservas@carcassonnepub.com.br>",
+      to,
+      subject: subject,
+      react: component,
+    });
+
+    if (data.error) {
+      return Response.json({
+        success: false,
+        error: data.error.error || "Erro desconhecido",
+      });
+    }
+
+    return Response.json({ success: true, data });
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      {
+        success: false,
+        error: error.message || "Erro desconhecido",
+      },
+      { status: 500 }
     );
-
-  const data = await resend.emails.send({
-    from: "Carcassonne Pub <reservas@carcassonnepub.com.br>",
-    to,
-    subject,
-    react: component,
-  });
-
-  return Response.json({ success: true, data });
+  }
 }
