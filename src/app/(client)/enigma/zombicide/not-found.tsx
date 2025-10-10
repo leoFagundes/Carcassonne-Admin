0995e93049f1ab3bcb1bd7c1a5ba24d8a6b811a2
leoFagundes <leofagundes2015@gmail.com>
2025-10-10 11:42:09 -0300

    enigma

--- src/app/(client)/enigma/zombicide/not-found.tsx
+++ src/app/(client)/enigma/zombicide/not-found.tsx
@@ -1,10 +1,11 @@
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center text-yellow-400">
+    <div className="flex flex-col items-center justify-center h-screen text-center text-yellow-400 p-4">
       <h1 className="text-4xl font-bold mb-4">🧟‍♂️ Página não encontrada!</h1>
       <p className="text-lg max-w-md">
-        Parece que os zumbis comeram esse conteúdo... tente voltar ao abrigo
-        antes que escureça!
+        Procurando por <strong>enigmas</strong>, talvez? Sinto lhe informar...
+        você tomou o caminho errado. Parece que os zumbis devoraram este
+        conteúdo. Volte ao abrigo antes que escureça!
       </p>
     </div>
   );
